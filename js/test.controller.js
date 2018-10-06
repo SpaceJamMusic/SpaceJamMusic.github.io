f56e9cc1fb81ce31e2d39fb3896b52098ef0736a
Cameron Stark <starkc920@outlook.com>
2018-10-05 20:33:21 -0400

    moved javascript code to html file

--- js/test.controller.js
+++ js/test.controller.js
@@ -32,29 +32,5 @@ angular.module('SpaceJam', ['spotify'])
             })
         }
 
-        window.onSpotifyWebPlaybackSDKReady = () => {
-            const token = $scope.token;
-
-            const player = new Spotify.player({
-                name: 'Space Jam Player',
-                getOAuthToken: cb => { cb(token); }
-            });
-
-            player.addListener('initialization_error', ({ message }) => { console.error(message); });
-            player.addListener('authentication_error', ({ message }) => { console.error(message); });
-            player.addListener('account_error', ({ message }) => { console.error(message); });
-            player.addListener('playback_error', ({ message }) => { console.error(message); });
-
-            player.addListener('player_state_changed', state => { console.log(state); });
-            player.addListener('ready', ({ device_id }) => {
-                console.log('Ready with Device ID', device_id);
-            });
-
-            player.addListener('not_ready', ({ device_id }) => {
-                console.log('Device ID has gone offline', device_id);
-            });
-
-            plater.connect();
-
-        }
+       
     }])
